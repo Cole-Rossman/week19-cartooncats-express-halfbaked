467d8c2a00f1703cfc57f9ccf7bcd6df79f6bf4c
Cole Rossman <colerossman99@gmail.com>
2022-06-06 12:11:15 -0700

    change in test for initial commit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,8 @@ describe('cats routes', () => {
   });
 
   it('/cats/:id should return cat detail', async () => {
-    const res = await request(app).get('/cats/1');
+    const res = await request(app).get('/cats/2');
+    // change route back to /cats/1
     const felix = {
       id: '1',
       name: 'Felix',
@@ -36,3 +37,4 @@ describe('cats routes', () => {
     pool.end();
   });
 });
+
